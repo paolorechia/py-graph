359425ef1f208acb96c1d78c82a6f104db1a0660
paolorechia <paolorechia@gmail.com>
2022-06-23 14:22:42 +0200

    Graph implementation

--- graph.py
+++ graph.py
@@ -24,15 +24,32 @@ class Graph:
     """Directed graph."""
 
     def __init__(self):
+        self._vertices = []
         pass
 
     def add_vertex(self, v: Vertex):
         raise NotImplementedError()
 
+    def add_edge(self, e: Edge):
+        raise NotImplementedError()
+
+    def outgoing_degree(self, v: Vertex):
+        raise NotImplementedError()
+
+    def _dfs_traverse(self, index, visited, hook, options):
+        raise NotImplementedError()
+
+    def dfs_traverse(self, starting_vertex, hook=None, options=None):
+        visited = set()
+        for i, v in enumerate(self._vertices):
+            if v.identifier == starting_vertex.identifier:
+                visited.add(i)
+                self._dfs_traverse(i, visited, hook, options)
+
 
 class LinkedListGraph(Graph):
     def __init__(self):
-        self._vertices = []
+        super().__init__()
         self._adjacency_lists = []
 
     def add_vertex(self, v: Vertex):
@@ -40,6 +57,7 @@ class LinkedListGraph(Graph):
         self._adjacency_lists.append([])
 
     def add_edge(self, e: Edge):
+        """This block could be improved with a hashmap/dict"""
         origin_index = -1
         destiny_index = -1
         i = 0
@@ -68,14 +86,12 @@ class LinkedListGraph(Graph):
                 return len(self._adjacency_lists[i])
         raise IndexError("Vertex not found")
 
-    def dfs_traverse(self, starting_vertex, hook=None, options=None):
-        visited = set()
-        for i, v in enumerate(self._vertices):
-            if v.identifier == starting_vertex.identifier:
-                visited.add(i)
-                self._dfs_traverse(i, visited, hook, options)
-
     def bfs_traverse(self, starting_vertex, hook=None, options=None):
+        """Should use a queue"""
+        raise NotImplementedError("Todo")
+
+    def dijkstra_traverse(self, starting_vertex, hook=None, options=None):
+        """Should use a priority queue"""
         raise NotImplementedError("Todo")
 
     def _dfs_traverse(self, index, visited, hook, options):
@@ -89,5 +105,60 @@ class LinkedListGraph(Graph):
 
 
 class MatrixGraph(Graph):
+    """Matrix-like non resizable graph"""
     def __init__(self, vertices: List[Vertex]):
-        self.vertices = []
+        self._vertices = vertices[:]
+        self._matrix = []
+        for i in range(len(self._vertices)):
+            self._matrix.append([])
+            for _ in range(len(self._vertices)):
+                self._matrix[i].append(0)
+
+    def add_edge(self, e: Edge):
+        """This block is copied from LinkedList and could be refactored,
+        to reduce code duplication
+        """
+        origin_index = -1
+        destiny_index = -1
+        i = 0
+        while i < len(self._vertices) and (
+            origin_index == -1 or destiny_index == -1
+        ):
+            v = self._vertices[i]
+            if v == e.origin:
+                origin_index = i
+            if v == e.destiny:
+                destiny_index = i
+            i += 1
+        if origin_index == -1:
+            raise IndexError(
+                "Cannot add edge: origin vertex not found in graph."
+            )
+        if destiny_index == -1:
+            raise IndexError(
+                "Cannot add edge: destiny vertex not found in graph."
+            )
+        # This line is where it differs from LinkedList
+        self._matrix[origin_index][destiny_index] = 1
+
+
+    def outgoing_degree(self, v: Vertex):
+        for i in range(len(self._vertices)):
+            if self._vertices[i].identifier == v.identifier:
+                degree = 0
+                for j in range(len(self._vertices)):
+                    degree += self._matrix[i][j]
+                return degree
+
+        raise IndexError("Vertex not found")
+
+    def _dfs_traverse(self, index, visited, hook, options):
+        origin = self._vertices[index]
+        if hook:
+            hook(origin)
+
+        for j in range(len(self._vertices)):
+            if self._matrix[index][j] == 1:
+                if j not in visited:
+                    visited.add(j)
+                    self._dfs_traverse(j, visited, hook, options)
--- test_graph.py
+++ test_graph.py
@@ -1,8 +1,8 @@
 import pytest
-from graph import LinkedListGraph, Vertex, Edge
+from graph import LinkedListGraph, MatrixGraph, Vertex, Edge
 
 
-def build_test_graph():
+def build_test_linked_list_graph():
     g = LinkedListGraph()
 
     A = Vertex("A")
@@ -21,8 +21,23 @@ def build_test_graph():
 
     return g, A, B, C, D, E
 
+def build_matrix_graph():
+
+    A = Vertex("A")
+    B = Vertex("B")
+    C = Vertex("C")
+    D = Vertex("D")
+    E = Vertex("E")
+
+    g = MatrixGraph([A, B, C, D, E])
+
+    g.add_edge(Edge(A, B))
+
+    return g, A, B, C, D, E
+
+
 def test_linked_list_graph_representation():
-    g, A, B, C, D, E = build_test_graph()
+    g, A, B, C, D, E = build_test_linked_list_graph()
 
     # Since it's a directed graph, we have an edge from A->B
     assert g.outgoing_degree(A) == 1
@@ -41,7 +56,47 @@ def test_linked_list_graph_representation():
 
 
 def test_linked_list_graph_traverse():
-    g, A, B, C, D, E = build_test_graph()
+    g, A, B, C, D, E = build_test_linked_list_graph()
+
+    l = []
+    def hook(v: Vertex):
+        l.append(v.identifier)
+        print(v.identifier)
+
+    g.dfs_traverse(A, hook)
+
+    assert l == ["A", "B"]
+    g.add_edge(Edge(A, B))
+    g.add_edge(Edge(A, C))
+    g.add_edge(Edge(C, D))
+    g.add_edge(Edge(D, E))
+        
+    l = []
+    g.dfs_traverse(A, hook)
+    assert l == ["A", "B", "C", "D", "E"]
+
+
+def test_graph_representation():
+    g, A, B, C, D, E = build_matrix_graph()
+
+    assert len(g._vertices) == 5
+    # Since it's a directed graph, we have an edge from A->B
+    assert g.outgoing_degree(A) == 1
+    # But we don't have one from B->A
+    assert g.outgoing_degree(B) == 0
+
+    g.add_edge(Edge(B, A))
+    # Now both should be one
+    assert g.outgoing_degree(A) == 1
+    # But we don't have one from B->A
+    assert g.outgoing_degree(B) == 1
+
+    g.add_edge(Edge(A, C))
+    assert g.outgoing_degree(A) == 2
+    
+
+def test_matrix_graph_traverse():
+    g, A, B, C, D, E = build_matrix_graph()
 
     l = []
     def hook(v: Vertex):
